9d5d30f971205fc338d137fcae64ae5badef792c
Mike Heft <mikeheft@gmail.com>
2017-08-08 11:54:29 -0600

    work on self.database

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,10 @@
 require 'sqlite3'
 
 class Task
-  attr_reader :title, :description
+  attr_reader :title, :description, :id
 
   def initialize(task_params)
+    @id          = task_params["id"] if task_params["id"]
     @description = task_params["description"]
     @title       = task_params["title"]
     @database    = SQLite3::Database.new('db/task_manager_development.db')
@@ -29,5 +30,11 @@ class Task
     database.results_as_hash = true
     task = database.execute("SELECT * FROM tasks WHERE id = ?", id.to_i).first
     Task.new(task)
+  end
+
+  def self.database
+    database = SQLite3::Database.new('db/task_manager_development.db')
+    database.results_as_hash = true
+    database
   end
 end
