259baa27537e3aea2a0b4b025237192c04ea9569
Mike Heft <mikeheft@gmail.com>
2017-08-08 11:57:38 -0600

    dry up code

--- app/models/task.rb
+++ app/models/task.rb
@@ -17,8 +17,6 @@ class Task
   end
 
   def self.all
-    database = SQLite3::Database.new('db/task_manager_development.db')
-    database.results_as_hash = true
     tasks = database.execute("SELECT * FROM tasks")
     tasks.map do |task|
       Task.new(task)
@@ -26,8 +24,6 @@ class Task
   end
 
   def self.find(id)
-    database = SQLite3::Database.new('db/task_manager_development.db')
-    database.results_as_hash = true
     task = database.execute("SELECT * FROM tasks WHERE id = ?", id.to_i).first
     Task.new(task)
   end
